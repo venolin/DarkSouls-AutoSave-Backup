ddea06dd67524c0d2002b18345c6295bb3eaf588
Venolin Naidoo <venolin1@gmail.com>
2019-09-30 00:46:29 +0200

    Upload of existing project.

---
+++ DarkSouls AutoSave Backup/Form1.Designer.vb
@@ -0,0 +1,97 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.fbdSelectDir = New System.Windows.Forms.FolderBrowserDialog()
+        Me.staMain = New System.Windows.Forms.StatusStrip()
+        Me.lblStatus = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.staMain.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'staMain
+        '
+        Me.staMain.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.lblStatus})
+        Me.staMain.Location = New System.Drawing.Point(0, 244)
+        Me.staMain.Name = "staMain"
+        Me.staMain.Size = New System.Drawing.Size(292, 22)
+        Me.staMain.TabIndex = 0
+        Me.staMain.Text = "StatusStrip1"
+        '
+        'lblStatus
+        '
+        Me.lblStatus.AutoSize = False
+        Me.lblStatus.Name = "lblStatus"
+        Me.lblStatus.Overflow = System.Windows.Forms.ToolStripItemOverflow.Never
+        Me.lblStatus.Size = New System.Drawing.Size(277, 17)
+        Me.lblStatus.Spring = True
+        Me.lblStatus.Text = "Path Selected: N/A"
+        Me.lblStatus.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'Label1
+        '
+        Me.Label1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label1.Font = New System.Drawing.Font("Baskerville Old Face", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.DarkRed
+        Me.Label1.Location = New System.Drawing.Point(98, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(182, 33)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "STOPPED ..."
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 5000000
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(292, 266)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.staMain)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "DarkSouls AutoSave Backup V1.0 Beta"
+        Me.staMain.ResumeLayout(False)
+        Me.staMain.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents fbdSelectDir As System.Windows.Forms.FolderBrowserDialog
+    Friend WithEvents staMain As System.Windows.Forms.StatusStrip
+    Friend WithEvents lblStatus As System.Windows.Forms.ToolStripStatusLabel
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+
+End Class
---
+++ DarkSouls AutoSave Backup/Form1.vb
@@ -0,0 +1,103 @@
+﻿Public Class Form1
+    Dim dir As String = "C:\"
+    Dim sw As IO.StreamWriter
+    Dim sr As IO.StreamReader
+    Dim config As DataTable
+    Dim lines As Array
+
+
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+        If IO.File.Exists(System.Windows.Forms.Application.StartupPath + "Ven.txt") Then
+            sr = New IO.StreamReader(System.Windows.Forms.Application.StartupPath + "Ven.txt")
+
+            Try
+                lines = sr.ReadLine.Split(vbTab)
+                dir = lines(1)
+                PopulateStatus()
+            Catch EmptyConfigFile As Exception
+                PopulateStatus()
+
+            End Try
+            sr.Close()
+
+        Else
+            sw = New IO.StreamWriter(System.Windows.Forms.Application.StartupPath + "Ven.txt")
+            Call ChangePath()
+        End If
+
+
+
+
+    End Sub
+
+    Public Function ChangePath()
+        Dim Bool As Boolean = False
+
+        If MessageBox.Show("Please select the location of the save game files", "Set Location", MessageBoxButtons.OKCancel) = Windows.Forms.DialogResult.OK Then
+            fbdSelectDir.ShowDialog()
+
+            Do While Bool = False
+                If IO.File.Exists(fbdSelectDir.SelectedPath + "DRAKS00005.sl2") = False Then
+                    MessageBox.Show("Dark Souls save game file does not exist in location. Please select a valid save game location", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                    fbdSelectDir.ShowDialog()
+                Else : Bool = True
+                End If
+            Loop
+
+
+            dir = fbdSelectDir.SelectedPath
+
+
+            PopulateStatus()
+            sw.WriteLine("Path:" + vbTab + dir)
+            sw.Flush()
+            sw.Close()
+        Else
+            sw.Flush()
+            sw.Close()
+        End If
+
+    End Function
+
+    Public Function PopulateStatus()
+        lblStatus.Text = "Path Selected: " + dir
+    End Function
+
+    Private Sub lblStatus_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles lblStatus.Click
+       
+
+        sw = New IO.StreamWriter(System.Windows.Forms.Application.StartupPath + "Ven.txt")
+        ChangePath()
+    End Sub
+
+    Private Sub Label1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label1.Click
+        If Label1.Text = "STOPPED ..." Then
+            Label1.Text = "RUNNING ..."
+            Timer1.Enabled = True
+            Timer1_Tick(sender, e)
+        Else : Label1.Text = "STOPPED ..."
+            Timer1.Enabled = False
+        End If
+
+    End Sub
+
+    Public Function RunBackup()
+
+        For i As Integer = 1 To 20
+            If IO.Directory.Exists(dir + CStr(Format(Today, "yyyy-MM-dd")) + " " + CStr(i)) = True Then
+            Else
+                IO.Directory.CreateDirectory(dir + CStr(Format(Today, "yyyy-MM-dd")) + " " + CStr(i))
+                IO.File.Copy(dir + "DRAKS00005.sl2", dir + CStr(Format(Today, "yyyy-MM-dd")) + " " + CStr(i) + "\DRAKS00005.sl2")
+                Exit For
+            End If
+
+        Next
+
+    End Function
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        RunBackup()
+    End Sub
+End Class
